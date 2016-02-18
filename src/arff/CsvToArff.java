7cea0afb0521c0e82c92eb5ff1270dabed188a5d
Nes280 <n.benichou@gmail.com>
2016-02-18 09:49:17 +0100

    csvToArff

---
+++ src/arff/CsvToArff.java
@@ -0,0 +1,72 @@
+package arff;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class CsvToArff {
+    public String DATAPATH = "./elements_projet/";
+
+    public CsvToArff() {
+    };
+
+    public String header() {
+        return "@relation donnees\n@attribut text STRING\n@attribut class {-1,1}\n@data";
+    }
+
+    public String lecture() {
+        String data = this.DATAPATH + "dataset.csv";
+        String label = this.DATAPATH + "labels.csv";
+        String resultat = this.header();
+        try {
+            // Ouverture de data
+            InputStream ipsData = new FileInputStream( data );
+            InputStreamReader ipsrData = new InputStreamReader( ipsData );
+            BufferedReader brData = new BufferedReader( ipsrData );
+
+            // Ouverture de label
+            InputStream ipsLabel = new FileInputStream( label );
+            InputStreamReader ipsrLabel = new InputStreamReader( ipsLabel );
+            BufferedReader brLabel = new BufferedReader( ipsrLabel );
+
+            String ligneData;
+            String ligneLabel;
+
+            while ( ( ligneData = brData.readLine() ) != null && ( ligneLabel = brLabel.readLine() ) != null ) {
+                resultat += "\n\"" + ligneData.replace( "\"", "\\\"" ) + "\"," + ligneLabel;
+
+            }
+            brData.close();
+            brLabel.close();
+        } catch ( Exception e ) {
+            System.out.println( e.toString() );
+        }
+        return resultat;
+    }
+
+    public void ecriture( String contenu, String nomFic ) {
+        try {
+            FileWriter fw = new FileWriter( nomFic );
+            BufferedWriter bw = new BufferedWriter( fw );
+            PrintWriter fichierSortie = new PrintWriter( bw );
+            fichierSortie.print( contenu );
+            fichierSortie.close();
+        } catch ( Exception e ) {
+            System.out.println( e.toString() );
+        }
+    }
+
+    public static void main( String[] args ) {
+
+        CsvToArff a = new CsvToArff();
+        String contenuArff = a.lecture();
+        a.ecriture( contenuArff, "result.arff" );
+        System.out.println( "ok" );
+
+    }
+
+}

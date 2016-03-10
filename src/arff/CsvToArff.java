e353f935fd1d6cadf8adfeab4ff077418ac9f382
Nes280 <n.benichou@gmail.com>
2016-03-10 14:20:36 +0100

    smiley et conf

--- src/arff/CsvToArff.java
+++ src/arff/CsvToArff.java
@@ -8,24 +8,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CsvToArff {
-    public String    DATAPATH    = "./elements_projet/";
-    public String[]  ponctuation = { ",", ".", "!" };   // , "!", "?"
-    public Hashtable smiley;
+    public String                    DATAPATH    = "./elements_projet/";
+    public String[]                  ponctuation = { ",", ".", "!" };   // ,
+                                                                        // "!",
+                                                                        // "?"
+    public Hashtable<String, String> smileyTwo;
+    public Hashtable<String, String> smileyThree;
+    public Hashtable<String, String> smileyTwoChar;
 
     public CsvToArff() {
-        smiley = new Hashtable();
-        smiley.put( ":)", "happy" );
-        smiley.put( ":-)", "happy" );
-        smiley.put( ":(", "sad" );
-        smiley.put( ":-(", "sad" );
-        smiley.put( ":D", "cheerful" );
-        smiley.put( ":-D", "cheerful" );
-        smiley.put( ":o)", "happy" );
-        smiley.put( ";)", "happy" );
-        smiley.put( ";-)", "happy" );
+        smileyTwo = new Hashtable();
+        smileyThree = new Hashtable();
+        smileyTwoChar = new Hashtable();
+        smileyTwo.put( ":)", "happy" );
+        smileyThree.put( ":-)", "happy" );
+        smileyThree.put( "q:)", "happy" );
+        smileyTwo.put( ":(", "sad" );
+        smileyThree.put( ":-(", "sad" );
+        smileyTwoChar.put( ":D", "cheerful" );
+        smileyThree.put( ":-D", "cheerful" );
+        smileyThree.put( ":o)", "happy" );
+        smileyTwo.put( ";)", "happy" );
+        smileyThree.put( ";-)", "happy" );
     };
 
     public String header() {
@@ -41,6 +49,21 @@ public class CsvToArff {
         for ( int i = 0; i < ponctuation.length; i++ ) {
             ligne = ligne.replace( ponctuation[i], " " );
         }
+        return ligne.replaceAll( "[ ]+", " " );
+    }
+
+    public String traitementSmiley( String ligne ) {
+
+        /*
+         * for ( Map.Entry<String, String> entry : smileyTwo.entrySet() ) {
+         * ligne = ligne.replace( entry.getKey(), " " + entry.getValue() + " "
+         * ); }
+         */
+
+        for ( Map.Entry<String, String> entry : smileyThree.entrySet() ) {
+            ligne = ligne.replace( entry.getKey(), " " + entry.getValue() + " " );
+        }
+
         return ligne.replaceAll( "[ ]+", " " );
     }
 
@@ -73,6 +96,14 @@ public class CsvToArff {
 
                 }
             }
+            if ( rep.equals( "s" ) ) {
+                while ( ( ligneData = brData.readLine() ) != null && ( ligneLabel = brLabel.readLine() ) != null ) {
+                    resultat += "\n\"" + echappementQuotes( traitementPonctuation( traitementSmiley( ligneData ) ) )
+                            + "\"," + ligneLabel;
+
+                }
+            }
+
             brData.close();
             brLabel.close();
         } catch ( Exception e ) {
@@ -95,7 +126,8 @@ public class CsvToArff {
 
     public static void main( String[] args ) {
         Scanner option = new Scanner( System.in );
-        System.out.println( "merci d'entrer l'option du fichier arff:\nb = text brut\np = sans ponctuation" );
+        System.out.println(
+                "merci d'entrer l'option du fichier arff:\nb = text brut\np = sans ponctuation\ns = sans smiley et sans ponctuation." );
         String rep = option.nextLine();
         CsvToArff a = new CsvToArff();
         String contenuArff = a.lecture( rep );
@@ -104,6 +136,8 @@ public class CsvToArff {
             nomFichier = "Brut.arff";
         if ( rep.equals( "p" ) )
             nomFichier = "Ponctuation.arff";
+        if ( rep.equals( "s" ) )
+            nomFichier = "PonctuationSmiley.arff";
 
         a.ecriture( contenuArff, nomFichier );
 
